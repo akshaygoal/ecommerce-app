01e9c2f7edd551053509ccde6f0766d8e07c0881
akshaygoal <akshaychikku262@gmail.com>
2024-06-16 19:10:46 +0530

    Active
    Active class

--- src/components/pages/Oversized T-shirts/Page1/DetailsPage/OversizedMoreData..js
+++ src/components/pages/Oversized T-shirts/Page1/DetailsPage/OversizedMoreData..js
@@ -6,12 +6,10 @@ import { useParams } from "react-router-dom";
 import Datas from "./DetaileData";
 import OversizedT2 from "../../Page2/OversizedT2";
 
-
-
 function OversizedMoreData() {
   const { id } = useParams(); // Extracting id from URL params
   const [data, setData] = useState(null);
-
+  const [activeSelector, setActiveSelector] = useState();
   useEffect(() => {
     // Filtering data based on the id
     const filteredData = Datas.find((item) => item.id === parseInt(id)); // Convert id to number
@@ -19,42 +17,68 @@ function OversizedMoreData() {
   }, [id]);
 
   if (!data) return <div>Loading...</div>;
-
+  function activeClickHandler(event) {
+    const value = event.target.value;
+    setActiveSelector(value);
+    console.log("activeSelector=>", value);
+  }
   return (
-    <div className="main" >
-    <div className="details-main-container">
-      <img src={data.image} alt="singleImage" className="product-image" />
-      <div className="Details-info">
-        <h2 className="title">{data.title}</h2>
-        <p className="price">{data.price}</p>
+    <div className="main">
+      <div className="details-main-container">
+        <img src={data.image} alt="singleImage" className="product-image" />
+        <div className="Details-info">
+          <h2 className="title">{data.title}</h2>
+          <p className="price">{data.price}</p>
 
-        <p className="tax-p">Tax included.</p>
+          <p className="tax-p">Tax included.</p>
 
-        <p className="offer">
-          <IoPricetagOutline className="offer-icons" /> Buy 2 get ₹100 off! code
-          -B2
-        </p>
-        <p className="offer">
-          <IoPricetagOutline className="offer-icons" /> Buy 4 Get 1 free (Add
-          any % to Cart)
-        </p>
-        <p className="offer">
-          <CiDeliveryTruck className="offer-icons" /> Free Shipping on all
-          prepaid orders
-        </p>
-        <p>Size</p>
-        <button className="size-btn">S</button>
-        <button className="size-btn">M</button>
-        <button className="size-btn">X</button>
-        <button className="size-btn">XL</button>
-        <br />
-        <button className="cart-btn">Add to cart</button>
+          <p className="offer">
+            <IoPricetagOutline className="offer-icons" /> Buy 2 get ₹100 off!
+            code -B2
+          </p>
+          <p className="offer">
+            <IoPricetagOutline className="offer-icons" /> Buy 4 Get 1 free (Add
+            any % to Cart)
+          </p>
+          <p className="offer">
+            <CiDeliveryTruck className="offer-icons" /> Free Shipping on all
+            prepaid orders
+          </p>
+          <p>Size</p>
+          <button
+            value="S"
+            onClick={activeClickHandler}
+            className={activeSelector == "S" ? "active" : "size-btn"}
+          >
+            S
+          </button>
+          <button
+            value="M"
+            onClick={activeClickHandler}
+            className={activeSelector == "M" ? "active" : "size-btn"}
+          >
+            M
+          </button>
+          <button
+            value="L"
+            onClick={activeClickHandler}
+            className={activeSelector == "L" ? "active" : "size-btn"}
+          >
+            L
+          </button>
+          <button
+            value="XL"
+            onClick={activeClickHandler}
+            className={activeSelector == "XL" ? "active" : "size-btn"}
+          >
+            XL
+          </button>
+          <br />
+          <button className="cart-btn">Add to cart</button>
+        </div>
       </div>
-     
+      <OversizedT2 />
     </div>
-    <OversizedT2/>
-    </div>
-   
   );
 }
 
